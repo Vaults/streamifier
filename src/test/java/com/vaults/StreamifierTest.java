fe0984411b1cd6123a2a2c05360e193f9edb28c1
Marcel Visser <marcel1337@hotmail.com>
2018-02-28 17:15:34 +0100

    Initial commit

---
+++ src/test/java/com/vaults/StreamifierTest.java
@@ -0,0 +1,22 @@
+package com.vaults;
+
+import org.junit.Test;
+
+public class StreamifierTest {
+
+    @Test
+    public void streamifyArray() {
+    }
+
+    @Test
+    public void streamifyCollection() {
+    }
+
+    @Test
+    public void streamifyEnumeration() {
+    }
+
+    @Test
+    public void streamifyEnum() {
+    }
+}
