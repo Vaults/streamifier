27dd4fa5fcfc42f453e7c031fcc0067abf996e9f
Marcel Visser <marcel1337@hotmail.com>
2018-03-01 09:20:53 +0100

    First test! Yay

--- src/test/java/com/vaults/StreamifierTest.java
+++ src/test/java/com/vaults/StreamifierTest.java
@@ -2,21 +2,35 @@ package com.vaults;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+import static com.vaults.Streamifier.streamify;
+import static org.junit.Assert.assertEquals;
+
 public class StreamifierTest {
 
     @Test
     public void streamifyArray() {
+        Object[] arr = new Object[]{"A", 1, new Exception()};
+        HashSet<Object> arrSet = new HashSet<>();
+        arrSet.addAll(Arrays.asList(arr));
+        assertEquals(streamify(arr).collect(Collectors.toSet()), arrSet);
     }
 
     @Test
     public void streamifyCollection() {
+
     }
 
     @Test
     public void streamifyEnumeration() {
+
     }
 
     @Test
     public void streamifyEnum() {
+
     }
 }
